56e94ea5c0376e87f96bc4bfd7194704120693d5
Gilberto <savoini@dac246.dac.unicamp.br>
2019-05-10 21:38:42 -0300

    search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -48,7 +48,26 @@ class Search extends Component {
             </li>
           ))}
         </ul>
+
       </Container>
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={this.afterOpenModal}
+        onRequestClose={this.closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+        <button onClick={this.closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
     );
   }
 }
